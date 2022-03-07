e0e3423db402f30eaf9bafec1d2a8cbc0eb78b5c
FinestArcadeArt <lukas.mutter@live.de>
2022-03-07 17:58:26 +0100

    rebuild

--- BTT SKR V1.4/Firmware/Marlin-2.0.x-SKR-V1.4-Turbo/Marlin/Configuration.h
+++ BTT SKR V1.4/Firmware/Marlin-2.0.x-SKR-V1.4-Turbo/Marlin/Configuration.h
@@ -110,7 +110,12 @@
  * Select a secondary serial port on the board to use for communication with the host.
  * :[-1, 0, 1, 2, 3, 4, 5, 6, 7]
  */
-#define SERIAL_PORT_2 0
+#define SERIAL_PORT_2 3
+
+/**
+ * Pinhack
+ */
+#define LPC_PINCFG_UART3_P4_28
 
 /**
  * This setting determines the communication speed of the printer.
@@ -121,7 +126,7 @@
  *
  * :[2400, 9600, 19200, 38400, 57600, 115200, 250000, 500000, 1000000]
  */
-#define BAUDRATE 115200
+#define BAUDRATE 250000
 
 // Enable the Bluetooth serial interface on AT90USB devices
 //#define BLUETOOTH
